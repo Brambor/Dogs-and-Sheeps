e0073e41382cec16a7b8229d531650441cdba363
Brambor <tondadrd@seznam.cz>
2015-07-23 00:38:18 +0200

    mistakes were made, now it's ok

--- values.py
+++ values.py
@@ -1,6 +1,6 @@
-map_x = 100 #Map width; any natural number higher than 3; default = 79
-map_y = 75 #Map height; any natural number higher than 3; default = 25
-FPS = 10 #Frames per second AND turns per second; any rational number higher than 3; default = 10
+map_x = 100 #Map width; any natural number higher than 2; default = 100
+map_y = 75 #Map height; any natural number higher than 2; default = 75
+FPS = 10 #Frames per second AND turns per second; any natural number higher than 0; default = 10
 Dogs = 2 #Starting numer of Dogs; any natural number; default = 2
 Sheep = 5 #Starting numer of Sheeps; any natural number; default = 5
 Grass = 25 #Starting numer of Grass; any natural number; default = 25

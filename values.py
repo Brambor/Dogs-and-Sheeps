78c28e6a8d9d7044f0109ab31f6e599e3835a0b7
Brambor <tondadrd@seznam.cz>
2015-07-23 00:31:26 +0200

    Upgraded to Graphic version! Now you need pygame...
    Added some pre-start settings (values.py) - Number of Sheep, Dogs, Grass

--- values.py
+++ values.py
@@ -1,3 +1,6 @@
-map_x = 79 #Map width; any natural number higher than 3; default = 79
-map_y = 25 #Map height; any natural number higher than 3; default = 25
+map_x = 100 #Map width; any natural number higher than 3; default = 79
+map_y = 75 #Map height; any natural number higher than 3; default = 25
 FPS = 10 #Frames per second AND turns per second; any rational number higher than 3; default = 10
+Dogs = 2 #Starting numer of Dogs; any natural number; default = 2
+Sheep = 5 #Starting numer of Sheeps; any natural number; default = 5
+Grass = 25 #Starting numer of Grass; any natural number; default = 25

b81ca5be5627e540bcecaa21908e1c7e8c562c61
Brambor <tondadrd@seznam.cz>
2023-11-26 21:13:05 +0100

    start graphical version with ENTER

--- simulation.py
+++ simulation.py
@@ -2,6 +2,22 @@ import sys
 
 from stuff import main, values
 
+def start(ver):
+	print(
+		"press\n"
+		"ESC to open menu\n"
+		"space to stop the simulation\n"
+		"\t'f' while stopped to move one frame forward\n\n"
+
+		"'a' to select all sheep\n"
+		"'d' to deselect all sheep\n"
+		"'t' to toggle selected on all sheep\n"
+		"click on a sheep to toggle selected\n\n"
+
+		"hower over a sheep to show the path it's taking\n"
+	)
+	main.Run(ver)
+
 picnames = ["Play","Quit"]
 ver = values.prefered_version
 if ver != "text":  # assuming graphic version
@@ -42,22 +58,11 @@ while True:
 				for button in buttons:
 					if button.rect.left < pos[0] < button.rect.right and button.rect.top < pos[1] < button.rect.bottom:
 						if button.name == "Play":
-							print(
-								"press\n"
-								"ESC to open menu\n"
-								"space to stop the simulation\n"
-								"\t'f' while stopped to move one frame forward\n\n"
-
-								"'a' to select all sheep\n"
-								"'d' to deselect all sheep\n"
-								"'t' to toggle selected on all sheep\n"
-								"click on a sheep to toggle selected\n\n"
-
-								"hower over a sheep to show the path it's taking\n"
-							)
-							main.Run(ver)
+							start(ver)
 						elif button.name == "Quit":
 							sys.exit()
+			elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+				start(ver)
 			elif event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):#() not needed
 				#print("Thanks for using!")
 				#sleep(2)
